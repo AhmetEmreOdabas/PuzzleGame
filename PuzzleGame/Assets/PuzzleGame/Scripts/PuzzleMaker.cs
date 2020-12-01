fd3a9336c26350b9465e8ab3cf76b4edaa0d7b2b
AhmetEmreOdabas <68646504+AhmetEmreOdabas@users.noreply.github.com>
2020-12-01 19:57:41 +0300

    new

---
+++ PuzzleGame/Assets/PuzzleGame/Scripts/PuzzleMaker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PuzzleMaker : MonoBehaviour
+{
+    public GameObject start;
+
+    public void SetPuzzleImage(Image image)
+    {
+        for (int i = 1; i < 10; i++)
+        {
+            GameObject.Find(i.ToString("0")).transform.Find("Puzzle").GetComponent<SpriteRenderer>().sprite = image.sprite;
+        }
+        start.SetActive(false);
+    }
+}
